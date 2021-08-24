7584c43f25c6017af62aee465a72e309516cc92c
louisaboy <louis_aboy@dlsu.edu.ph>
2021-08-24 13:17:23 +0800

    added in routes

--- routes/routes.js
+++ routes/routes.js
@@ -4,7 +4,19 @@ const loginController = require('../controller/loginController.js');
 const signupController = require('../controller/signupController.js');
 const app = express();
 
+// controller
 app.get('/favicon.ico', controller.getFavicon);
 app.get('/', controller.getIndex);
+
+// loginController
+app.get('/login/:id', loginController.getLogin);
+app.get('/login', loginController.postLogin);
+
+// signupController
+app.get('/signup', signupController.getSignUp);
+app.get('/signup', signupController.postSignUp)
+
+// aboutUsController
+// app.get('/aboutus', aboutUsController);
 
 module.exports = app;
