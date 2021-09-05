6d2a4de04875ddb89a34a7240cedb7ce758c0307
izskat <nicholas_scott_campos@dlsu.edu.ph>
2021-09-05 23:23:11 +0800

    studying cartcontroller

--- controller/cartController.js
+++ controller/cartController.js
@@ -3,6 +3,17 @@ const Cart = require('../model/about.js');
 
 const cartController = {
     getCart: function(req,res){
+        console.log("Cart is running");
+        var e = req.session.email;
+        let cart = []
+        var query1 = {email: e};
+        console.log("Checking cart for " + e + "...");
+            db.findMany(Cart, query1, {_id:-1}, null, 0, function(x){
+                
+            });
+
+        
+
         res.render('cart', {success:"hidden"});
     },
 
--- model/cart.js
+++ model/cart.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const cartSchema = new mongoose.Schema({
-    user:{
+    email:{
         type: String,
         required: true,
     },
