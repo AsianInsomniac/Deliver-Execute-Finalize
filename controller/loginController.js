01865c5242bdd682daa6bc6119b16489df14b118
louisaboy <louis_aboy@dlsu.edu.ph>
2021-09-14 16:06:46 +0800

    Photo Gallery position CSS fix

--- controller/loginController.js
+++ controller/loginController.js
@@ -2,7 +2,11 @@ const db = require('../model/db.js');
 
 const bcrypt = require('bcryptjs');
 
+// const popup = require('popups');
+
 const User = require('../model/user.js');
+
+// const loginModal = document.getElementById('id01')
 
 const loginController = {
     getLogin: function (req, res){
@@ -22,7 +26,7 @@ const loginController = {
 		console.log ("Logging in " + e + " ...");
         var query1 = {email: e};
 			db.findOne(User, query1, null, function(x) {
-				
+				console.log("went here pa rin");
 				if(x)
 					bcrypt.compare(p, x.password, function(err, equal) {
 						
@@ -34,13 +38,17 @@ const loginController = {
 
 							res.render('home', {email: x.email});
 						}
-						// else{
-						// 	res.render('home');
-						// }
+						else{
+							console.log("Password not found!");
+							alert('Username or Password is not found!');
+						}
 						
 					});
-				// else
-				// 	res.render('home');
+				else {
+					console.log("invalid user or pass");
+					res.render('home' ,{openloginmodal: true});
+				}
+					
         });
     }
 }
